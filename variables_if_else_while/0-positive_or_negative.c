1e4f277309c1a3a954ea9fe9aabd28ae2a13531a
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-20 15:33:55 +0200

    Task 1

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - Print if a random number is positive, zero or negative
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
@@ -16,10 +18,10 @@ int main(void)
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
-	}else if (n == 0)
+	} else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	}else (n < 0)
+	} else
 	{
 		printf("%d is negative\n", n);
 	}
