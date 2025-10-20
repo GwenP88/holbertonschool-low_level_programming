0ae2fff4b3b37cd1b857f737a6253855c9f2f20a
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-20 16:41:02 +0200

    Task 5

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Print the list of single digit numbers of base 10 (prinf is forbiden)
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+	return (0);
+}
