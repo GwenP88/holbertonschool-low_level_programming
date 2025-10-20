e251a027021931ddbb0fcb6407ea2a554673b278
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-20 15:48:08 +0200

    Task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i = 0;
+
+	for (i = 'a'; i <= 'z' ; i++)
+	{
+		putchar (i);
+	};
+
+	putchar ("\n");
+	return (0);
+}
