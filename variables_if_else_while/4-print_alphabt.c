a200d7681e4211a94a09ece09760fe31f9895f01
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-20 16:30:12 +0200

    Task 3

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet in lowercase and don't print q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i = 0;
+
+
+	for (i = 'a'; i <= 'z' ; i++)
+	{
+		if (i != 'q' && i != 'e')
+		{
+			putchar (i);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
