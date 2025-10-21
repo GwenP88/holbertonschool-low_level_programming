fcb062d6e64af6619eeb1b19d82a1c10048a0a35
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-21 12:43:56 +0200

    Task 12

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -21,18 +21,17 @@ int main(void)
 	{
 		for (b = a + 1 ; b <= 99 ; b++)
 		{
-			putchar(a / 10);
-			putchar(a % 10);
+			putchar('0' + (a / 10));
+			putchar('0' + (a % 10));
 			putchar(' ');
-			putchar(b / 10);
-			putchar(a % 10);
+			putchar('0' + (b / 10));
+			putchar('0' + (b % 10));
 
 			if (!(a == 98 && b == 99))
 			{
 				putchar(',');
 				putchar(' ');
 			}
-
 		}
 	}
 
