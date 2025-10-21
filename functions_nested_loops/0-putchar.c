879521e746a7e58223775f256925b066c03bf6f6
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-21 15:25:25 +0200

    initialisation projet 3

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
- * main -
+ * main - print _putchar followed by a new line
+ * printf, puts... is forbiden
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-puts("_putchar\n");
-return (0);
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
 }
