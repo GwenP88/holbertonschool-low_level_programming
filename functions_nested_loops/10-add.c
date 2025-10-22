8512b8fb125d2732339ead2e2ea4ab669ca3f679
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-22 16:50:35 +0200

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result
+ *
+ * @a: is a first integer
+ * @b: is a second inteeger
+ *
+ * Return: the result of adds
+ *
+ */
+
+int add(int a, int b)
+{
+
+	int resultat = a + b;
+
+	return (resultat);
+}
