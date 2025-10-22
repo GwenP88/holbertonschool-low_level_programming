dbf2acd4319aad9d443e8fdf90a00d648bb97af7
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-22 16:31:32 +0200

    task 9

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -8,7 +8,6 @@
 
 void print_alphabet(void)
 {
-
 	char i = 0;
 
 	for (i = 'a'; i <= 'z' ; i++)
---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints the 9 times table, starting with 0.
+ *
+ * Return: the 9 times table
+ *
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int n;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		for (j = 0; j < 10 ; j++)
+		{
+			n = i * j;
+
+			if (j == 0)
+			{
+				_putchar('0' + n);
+
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (n < 10)
+				{
+					_putchar(' ');
+					_putchar('0' + n);
+
+				}
+				else
+				{
+					_putchar('0' + (n / 10));
+					_putchar('0' + (n % 10));
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
