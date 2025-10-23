0db1a1786943f979600dbf56b0a0057c83dce82a
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-23 14:31:39 +0200

    task 12

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,38 +1,52 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_times_table - function that prints the n times table, starting with 0.
- *
  * @n: integer in parametre  of function
- *
  * Return: the 9 times table
- *
  */
 
 void print_times_table(int n)
 {
-	int i;
-	int j;
+	int i, j, res_multi;
 
 	if (n < 0 || n > 15)
-	{
 		return;
-	}
-	else
+
+	for (i = 0; i <= n ; i++)
 	{
-		for (i = 0; i <= n; i++)
+		for (j = 0; j <= n ; j++)
 		{
-			for (j = 0; j <= n; j++)
+			res_multi = i *j;
+
+			if (j == 0)
+				_putchar('0' + res_multi);
+			else
 			{
-				printf("%3d", i * j);
-				if (j < n)
+				_putchar(',');
+				_putchar(' ');
+
+				if (res_multi < 10)
 				{
-					printf(", ");
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + res_multi);
+				}
+				else if (res_multi < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + (res_multi / 10));
+					_putchar('0' + (res_multi % 10));
+				}
+				else
+				{
+					_putchar('0' + (res_multi / 100));
+					_putchar('0' + ((res_multi / 10) % 10));
+					_putchar('0' + (res_multi % 10));
 				}
 			}
-			printf("\n");
 		}
+		_putchar('\n');
 	}
-	printf("\n");
+	_putchar('\n');
 }
