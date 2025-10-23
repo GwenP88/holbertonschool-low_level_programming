790457abba4f7c10c1618685ae904d9f04e2f4d2
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-23 12:02:15 +0200

    task 10

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,11 +2,10 @@
 #include "main.h"
 
 /**
- * void print_to_98 -  function that prints all natural numbers from n to 98,
+ * print_to_98 -  function that prints all natural numbers from n to 98,
  * followed by a new line.
  *
  * @n: is a number passed as a parameter
- * @b: is a second inteeger
  *
  * Return: the result of adds
  *
@@ -14,68 +13,21 @@
 
 void print_to_98(int n)
 {
-	int i;
-
-	if (n <= 98)
+	if (n >= 98)
 	{
-		for (i = n ; i <= 98 ; i++)
+		while (n > 98)
 		{
-			int nbr = i;
-
-			if (i < -9)
-			{
-				_putchar('-');
-				nbr = -nbr;
-
-				_putchar('0' + (nbr / 10));
-				_putchar('0' + (nbr % 10));
-
-			} else if (i < 0)
-			{
-				_putchar('-');
-				nbr = -nbr;
-				_putchar('0' +  nbr);
-			} else if (i < 10)
-			{
-				_putchar('0' + nbr);
-			} else if (i < 100)
-			{
-				_putchar('0' + (nbr / 10));
-				_putchar('0' + (nbr % 10));
-			} else if (i >= 100)
-			{
-				_putchar('0' + (nbr / 100));
-				_putchar('0' + ((nbr / 10) % 10));
-				_putchar('0' + (nbr % 10));
-			}
-			if (!(i == 98))
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			printf("%d, ", n--);
 		}
-	} else if (n > 98)
-	{
-		for (i = n ; i >= 98 ; i--)
-		{
-			int nbr = i;
-
-			if (i >= 98 && i < 100)
-			{
-				_putchar('0' + (nbr / 10));
-				_putchar('0' + (nbr % 10));
-			} else if (i >= 100)
-			{
-				_putchar('0' + (nbr / 100));
-				_putchar('0' + ((nbr / 10) % 10));
-				_putchar('0' + (nbr % 10));
-			}
-			if (!(i == 98))
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
+		printf("%d\n", n);
 	}
-	_putchar('\n');
+
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n++);
+		}
+		printf("%d\n", n);
+	}
 }
