3cb2b05daff77948a69422ddd6c558b07434391e
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-23 14:38:47 +0200

    task 12

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -12,8 +12,9 @@ void print_times_table(int n)
 
 	if (n < 0 || n > 15)
 		return;
-
-	for (i = 0; i <= n ; i++)
+	else
+	{
+		for (i = 0; i <= n ; i++)
 	{
 		for (j = 0; j <= n ; j++)
 		{
@@ -49,4 +50,5 @@ void print_times_table(int n)
 		_putchar('\n');
 	}
 	_putchar('\n');
+	}
 }
