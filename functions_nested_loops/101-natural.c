9b6a4716b71b1262330c14b747bfddb43e56338a
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-23 16:27:50 +0200

    task 12

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i = 0;
+	long sum = 0;
+
+	for (i = 0 ; i < 1024 ; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum = +i;
+		}
+	}
+	printf("%ld", sum);;
+
+	return (0);
+}
+
