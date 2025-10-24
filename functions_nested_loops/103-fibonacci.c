0e1b27891307bb65982395a7523673e86b5a623b
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-24 10:16:36 +0200

    task 12

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -32,3 +32,30 @@ int main(void)
 	printf("\n");
 	return (0);
 }
+
+/*
+int main(void)
+
+    long a = 0;
+    long b = 1;
+    long sum = 0;       prochain terme de Fibonacci
+    long sum_pair = 0;  somme des termes pairs
+
+    while (1) veut dire boucle infinie (identique à for (;;) ou while (true))
+    {
+        sum = a + b;            génère le prochain terme
+
+        if (sum > 4000000)      borne sur le terme que tu ajoutes
+            break;
+
+        if (sum % 2 == 0)       si pair, on l’additionne
+            sum_pair += sum;
+
+        a = b;                  avance la suite
+        b = sum;
+    }
+
+    printf("%ld\n", sum_pair);
+    return (0);
+}
+*/
---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long a = 0;
+	long b = 1;
+	long sum;
+	int i;
+
+
+	for (i = 0; i < 98 ; i++)
+	{
+		sum = a + b;
+		printf("%ld", sum);
+		a = b;
+		b = sum;
+
+		if (i < 97)
+			printf(", ");
+	}
+	printf("\n");
+
+	return (0);
+}
