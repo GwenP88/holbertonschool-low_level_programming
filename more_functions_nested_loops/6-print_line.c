49dbc575b27a7932ee8f72b6245823c933a07e86
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-27 12:20:23 +0100

    initialisation more_functions_nested_loops

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line in the terminal.
+ * @n: the number of times the character _ should be printed.
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i <= n && i >= 0)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
