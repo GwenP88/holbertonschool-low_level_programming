d5c4d16fa4fe187deb1ca4f6d0d16fe66f3c57ff
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-28 14:32:44 +0100

    task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,31 @@
 #include "main.h"
 
 /**
- * print_rev - function that prints a string, in reverse, followed by a new line.
+ * rev_string - unction that reverses a string.
+ * followed by a new line.
  * @s: a pointer to a string;
  */
 
-void print_rev(char *s)
+void rev_string(char *s)
 {
+	char *start = s;
+	char *end;
+	char tmp;
 
+	while (*s != '\0')
+	{
+		s++;
+	}
+
+	end = s - 1;
+
+	while (start < end)
+	{
+		tmp = *start;
+		*start = *end;
+		*end = tmp;
+		start ++;
+		end --;
+	}
 }
+
