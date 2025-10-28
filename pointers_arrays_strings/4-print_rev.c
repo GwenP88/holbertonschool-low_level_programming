61fdf1f1e0d0826a1ad57263f799af581f732a0c
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-28 13:18:01 +0100

    task 3

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string, in reverse, followed by a new line.
+ * @s: a pointer to a string;
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int j = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = i - 1; j >= 0 ; j--)
+			_putchar(s[j]);
+	_putchar('\n');
+}
---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string, in reverse, followed by a new line.
+ * @s: a pointer to a string;
+ */
+
+void print_rev(char *s)
+{
+
+}
