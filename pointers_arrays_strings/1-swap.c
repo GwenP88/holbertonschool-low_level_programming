a55fe75bfeba0a322c2df2580d6e1654aea8cd16
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-28 10:16:54 +0100

    task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values of two integers.
+ * @a: a pointer to the first integer
+ * @b: a pointer to the first integer
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
