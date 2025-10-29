5285498f948a7afac248829b32e13a0aa9379acc
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-29 09:58:42 +0100

    task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - function that copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest.
+ * @dest: address where the copied string will be stored;
+ * @src: address where the source string is stored;
+ * Return: the copy of the sting pointed to by src;
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return(dest);
+}
