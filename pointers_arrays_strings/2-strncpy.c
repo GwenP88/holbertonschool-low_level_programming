00950d171037b2f06aa8feb1a14dd3cbbb361a05
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-29 16:38:43 +0100

    task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies the string pointed to by src
+ * @dest: address where the copied string will be stored;
+ * @src: address where the source string is stored;
+ * @n:numbers of bytes of src.
+ * Return: the copy of the sting pointed to by src;
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
