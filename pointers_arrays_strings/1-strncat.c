152908f5b1ba0f8043b934743f6ed229a8112bcf
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-29 15:53:15 +0100

    task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncat - function that concatenates two strings.
+ *
+ * @dest: destination string
+ * @src: source string
+ *
+ *
+ * Return: a pointer to the resulting string dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '0')
+	{
+		i++;
+	}
+
+
+	while (src[j] != '\0' && j <= n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	dest[i] = '\n';
+	return (dest);
+}
