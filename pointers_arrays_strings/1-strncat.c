797d5792629104cf89b799656331b1fdc04f0627
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-29 15:58:39 +0100

    task 1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -15,18 +15,17 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 
-	while (dest[i] != '0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
-	while (src[j] != '\0' && j <= n)
+	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
 	dest[i] = '\0';
-	dest[i] = '\n';
 	return (dest);
 }
