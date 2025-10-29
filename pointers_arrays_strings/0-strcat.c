a590d0f396fc2e2c15ebe0438eed6e99dfadf8a1
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-29 14:02:51 +0100

    initialisation projet 3

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcat - function appends the src string to the dest string,
+ * overwriting the terminating null byte (\0) at the end of dest,
+ * and then adds a terminating null byte
+ *
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: a pointer to the resulting string dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j;
+
+	while ( dest[i] == '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; src[j] <= '\0'; j++)
+	{
+		dest[i] =  src[i];
+		j++;
+		i++;
+	}
+	return (dest);
+}
+
