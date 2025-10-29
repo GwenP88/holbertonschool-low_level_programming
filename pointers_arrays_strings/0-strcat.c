4618869d2de5af281bf9b44f17108922175bcbc6
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-29 14:15:42 +0100

    initialisation projet 3

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -21,13 +21,12 @@ char *_strcat(char *dest, char *src)
 		i++;
 	}
 
-	for (j = 0; src[j] < '\0'; j++)
+	for (j = 0; src[j] != '\0'; j++)
 	{
-		dest[i] = src[i];
+		dest[i] = src[j];
 		i++;
 	}
 	dest[i] = '\0';
 
 	return (dest);
 }
-
