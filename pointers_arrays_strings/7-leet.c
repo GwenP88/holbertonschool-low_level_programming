87a2c2a3f7dd46677be681a54e8850d9cee1f02d
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-31 10:26:26 +0100

    task 6

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * leet -  function that encodes a string into 1337.
+ * @s: a string
+ * Return: the new encoded string
+ */
+
+char *leet(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == 'a' || s[i] == 'A')
+		{
+			s[i] = '4';
+		}
+
+		if (s[i] == 'e' || s[i] == 'E')
+		{
+			s[i] = '3';
+		}
+
+		if (s[i] == 'o' || s[i] == 'O')
+		{
+			s[i] = '0';
+		}
+
+		if (s[i] == 't' || s[i] == 'T')
+		{
+			s[i] = '7';
+		}
+
+		if (s[i] == 'l' || s[i] == 'L')
+		{
+			s[i] = '1';
+		}
+
+		i++;
+	}
+	return (s);
+}
+
