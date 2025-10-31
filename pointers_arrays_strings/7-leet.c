b3e6eab1a2874944dee784569a3b080c568e1822
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-10-31 12:18:35 +0100

    task 6

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,36 +8,19 @@
 
 char *leet(char *s)
 {
-	int i = 0;
+	int i, j;
+	const char *lettre = "aAeEoOlLtT";
+	const char *chiffre = "4433001177";
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
+		for (j = 0; j < lettre[j] ; j++)
 		{
-			s[i] = '4';
+			if (s[i] == lettre[j])
+			{
+				s[i] = chiffre[j];
+			}
 		}
-
-		if (s[i] == 'e' || s[i] == 'E')
-		{
-			s[i] = '3';
-		}
-
-		if (s[i] == 'o' || s[i] == 'O')
-		{
-			s[i] = '0';
-		}
-
-		if (s[i] == 't' || s[i] == 'T')
-		{
-			s[i] = '7';
-		}
-
-		if (s[i] == 'l' || s[i] == 'L')
-		{
-			s[i] = '1';
-		}
-
-		i++;
 	}
 	return (s);
 }
