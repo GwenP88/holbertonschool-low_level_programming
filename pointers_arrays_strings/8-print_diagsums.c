72b04898339bd8538987d723460d03f9fd458693
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-11-04 09:44:51 +0100

    task 5

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,14 +1,29 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_diagsums - function that prints the sum of the two diagonals
  * of a square matrix of integers.
- * @a: pointer to a interger
+ * @a: pointer to an array of integers
  * @size: the size of the square matrix
+ * (the number of row and col because it's a square)
+ * Return: nothing because void function
  */
 
 void print_diagsums(int *a, int size)
 {
 	int i;
+	int diag1 = 0, diag2 = 0;
 
+	for (i = 0; i < size; i++)
+	{
+		/**
+		 * le numero de case est toujour a[i * size + j]
+		 * a[i * size + j] == a[index]
+		 */
+
+		diag1 = diag1 + a[i * size + i];
+		diag2 = diag2 + a[i * size + (size - 1 - i)];
+	}
+	printf("%d, %d\n", diag1, diag2);
 }
