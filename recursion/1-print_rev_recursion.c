471a19bd31efc4c4b8a7b2bc50a306dafae174f7
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-11-04 11:47:35 +0100

    task 0

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -11,9 +11,17 @@ void _print_rev_recursion(char *s)
 	if (*s == '\0')
 	{
 		return;
+		/* si le caractère courant est le fin de chaine
+		* alors on quitte
+		*/
 	} else if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
+		/** on applique d'abord la récursivité pour aller au
+		* bout de la chaine (haut de pile)
+		*/
 		_putchar(*s);
+		/*on affiche les caractère un a un en redescandant
+		*/
 	}
 }
