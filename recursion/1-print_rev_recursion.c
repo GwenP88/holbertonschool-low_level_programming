26f2dbfb7b50eec783b90087de616c205505525b
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-11-04 10:51:04 +0100

    task 0

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _memset - function that fills memory with a constant byte.
+ * @s: memory area pointed
+ * @b: constant byte
+ * @n: number of bytes
+ * Return: the memory area pointed
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+}
