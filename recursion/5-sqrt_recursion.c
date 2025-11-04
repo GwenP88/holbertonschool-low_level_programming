0e54ef84f69bb1142ad0aa6c496a9f4153a2ce96
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-11-04 14:02:09 +0100

    task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,14 +1,29 @@
 #include "main.h"
 
 /**
- * _memset - function that fills memory with a constant byte.
- * @s: memory area pointed
- * @b: constant byte
- * @n: number of bytes
- * Return: the memory area pointed
+ * factorial - function that returns the natural square root
+ * of a number.
+ * @n: a given interger
+ * Return: the natural square root of a given number
+ * or -1 to indicate an error, if the number doesn't have a
+ * natural square root.
  */
+int test(int n, int i);
 
-char *_memset(char *s, char b, unsigned int n)
+int _sqrt_recursion(int n)
 {
+	if (n < 0)
+        return (-1);
+    if (n == 1 || n == 0)
+        return (n);
+    return (test(n, 1));
+}
 
+int test(int n, int i)
+{
+    if (i * i == n)
+        return (i);
+    if (i * i > n)
+        return (-1);
+    return (test(n, i + 1));
 }
