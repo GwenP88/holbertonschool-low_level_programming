782c9d9f32101f710fd105b4fd3bcc15ffed7a6b
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-11-05 18:46:46 +0100

    task 3

---
+++ argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - a program that adds positive numbers.
+ * @argc: total number of arguments (including the programme name).
+ * @argv: array of strings containing the arguments.
+ * Return: 0 if everything is OK.
+ */
+
+int main(int argc, char *argv[])
+{
+
+    int sum = 0;
+
+    for (int i = 1; i < argc; i++)
+    {
+	}
+
+    printf("%d\n", sum);
+
+    return 0;
+}
+
+
