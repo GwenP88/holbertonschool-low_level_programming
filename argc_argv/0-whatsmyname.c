3eab3e1f5364ad3fa907b9534d9b294fbfe17391
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-11-05 08:15:36 +0100

    init repository argc_argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function that prints a string, followed by a new line.
+ * @argc: a pointed string
+ * @argv: a pointed string
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* === Prototypes === */
+
+int _putchar(char c);
+
+#endif
