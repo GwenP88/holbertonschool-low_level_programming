27adae09c150cfe6b895e4870cb0b6c035e8453a
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-11-05 08:43:03 +0100

    task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -14,14 +14,14 @@ int main(int argc, char *argv[])
 	int a, b;
 
 	if (argc != 3)
-    {
-        printf("Error\n");
-        return 1;
-    }
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    a = atoi(argv[1]);
-    b = atoi(argv[2]);
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
 
-    printf("%d\n", a * b);
-    return 0;
+	printf("%d\n", a * b);
+	return (0);
 }
