3d9ae4ab1d637c2173c8584024e20bd7cf05d4b1
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-11-06 11:53:03 +0100

    task 3

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -12,15 +12,25 @@
 
 int main(int argc, char *argv[])
 {
-
+	int i, j;
     int sum = 0;
 
-    for (int i = 1; i < argc; i++)
+	if ( argc == 1)
+		printf("0\n");
+
+    for (i = 1; i < argc; i++)
     {
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+				sum += atoi(*argv);
+		}
+		printf("%d\n", sum);
 	}
-
-    printf("%d\n", sum);
-
     return 0;
 }
 
