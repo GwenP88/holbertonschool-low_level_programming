16ad6443f2da811bf1bef5f3a381dbcd9d9ee3b3
GwenP88 <148625456+GwenP88@users.noreply.github.com>
2025-11-11 10:49:29 +0100

    task 2 - project 2 - more_malloc_free

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -12,5 +12,23 @@
 
 int *array_range(int min, int max)
 {
+	int *array = NULL;
+	int array_size;
+	int i;
 
+	if (min > max)
+		return (NULL);
+
+	array_size = (max - min + 1);
+
+	array = malloc(array_size * sizeof(int));
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < array_size; i++)
+		array[i] = min + i;
+
+	return (array);
+	free(array);
 }
