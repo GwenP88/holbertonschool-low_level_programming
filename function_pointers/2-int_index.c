e5d04a555a49831f73ada35012464d9f9be52cba
GwenP88 <12777@holbertonstudents.com>
2025-11-17 12:29:22 +0100

    TASK 2 - V2 - projet function_pointers

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -20,15 +20,31 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 
+	/* Vérification des paramètres d’entrée :
+	 * - size <= 0 : taille invalide
+	 * - array == NULL : tableau inexistant
+	 * - cmp == NULL : fonction de comparaison inexistante
+	*/
+
 	i = 0;
 
 	while (i < size)
 	{
 		result = cmp(array[i]);
+		/* Appel de la fonction de comparaison sur l’élément courant du tableau.
+		 * cmp retourne :
+		 *   - 0  : si la condition n’est pas remplie
+		 *   - !=0: si la condition est remplie pour array[i]
+		 */
 
 		if (result != 0)
 			return (i);
+			/* Si fct cmp donne une valeur non nulle, on a trouvé
+		 	* le premier élément qui vérifie la condition → on renvoie son index.
+		 	*/
 		i++;
+		/* Sinon, on passe à l’élément suivant */
 	}
 	return (-1);
+	/* Si fin de boucle et aucun élément trouvé alors -1*/
 }
