d7ef6b362fd89a6f69a0242909d7b54e88e5a8ca
GwenP88 <12777@holbertonstudents.com>
2025-11-19 18:41:10 +0100

    task 1 - V1

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - function that print strings in parameters.
+ *
+ * @separator: pointer to the first caracter of the string
+ * to be printed between strings
+ * @n: the number of interger on parameters
+ *
+ * Return: nothing (void function)
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strings;
+	unsigned int i = 0;
+	char *list;
+
+	va_start(strings, n);
+
+	while (i < n)
+	{
+		list = va_arg(strings, char*);
+		printf("%s", list);
+
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+
+		i++;
+	}
+	printf("\n");
+	va_end(strings);
+}
