3fe087774d9062e87fd8fe864f215688d8590442
GwenP88 <12777@holbertonstudents.com>
2025-11-20 22:02:14 +0100

    task atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -28,8 +28,15 @@ int _atoi(char *s)
 	while (s[i] >= '0' && s[i] <= '9')
 	{
 		digit = (s[i] - '0');
-		result = (result * 10) + digit;
-		i++;
+		if (signe == -1)
+		{
+			result = (result * 10) - digit;
+			i++;
+		} else
+		{
+			result = (result * 10) + digit;
+			i++;
+		}
 	}
-	return (result * signe);
+	return (result);
 }
