95384301ca1220d5434870dab6df4e9bb9283e2f
GwenP88 <12777@holbertonstudents.com>
2025-12-01 16:07:12 +0100

    Project C - Singly linked lists - task 3 - V1

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,13 +1,42 @@
 #include "lists.h"
 
 /**
- * add_node_end -
- * @head:
- * @str:
- * Return:
+ * add_node_end - function that adds a new node at the end of a list.
+ * @head: first element onf list.
+ * @str: the new element to add
+ * Return: the address of the new element, or NULL if it failed
  */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *new;
+	list_t *current = *head;
+	int i = 0;
 
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	new->len = i;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	current->next = new;
+	return (new);
 }
