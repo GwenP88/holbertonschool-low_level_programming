e716a06d9ab1344d95e13e9371c5824fb3883a5f
GwenP88 <12777@holbertonstudents.com>
2025-12-01 14:37:47 +0100

    Project C - Singly linked lists - task 1 - V1

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -6,26 +6,38 @@
  * Return: the number of nodes
  */
 
+/* fonction qui prend en paramètre un pointeur const vers le head h d'une liste. */
+/* renvoie un size_t représentant le nb de noeud*/
 size_t print_list(const list_t *h)
 {
-	size_t n = 0;
-	const list_t *current;
+	size_t n = 0; /* Init du compteur de noeud*/
+	const list_t *current; /* décl d'un pointeur élement courant*/
 
 	if (h == NULL)
 		return (0);
+	/*Teste si la tête de liste est NULL → la liste est vide. Renvoi 0 noeud*/
 
 	current = h;
+	/*Initialise current avec la tête de liste*/
 
 	while (current != NULL)
+	/*Boucle tant que le pointeur current pointe vers un nœud valide*/
 	{
 		if (current->str != NULL)
+		/*Vérifie si la chaîne stockée dans le nœud courant (str) est non NULL.*/
 		{
-			printf("[%d] ", current->len);
+			printf("[%u] ", current->len);
+			/*Affiche la longueur de la chaîne du nœud courant entre crochets, suivie d’un espace*/
 			printf("%s\n", current->str);
+			/*Affiche la chaîne elle-même, suivie d’un retour à la ligne*/
 		} else
 			printf("[0] (nil)\n");
+			/*Affiche le format imposé par l’énoncé pour les nœuds dont la chaîne est NULL.*/
 		n++;
+		/*Incrémente le compteur de nœuds à chaque itération*/
 		current = current->next;
+		/*Fait avancer le pointeur de parcours vers le nœud suivant dans la liste*/
 	}
 	return (n);
+	/*Renvoie le nombre total de nœuds*/
 }
--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,12 +1,23 @@
 #include "lists.h"
 
 /**
- * list_len -
- * @h:
- * Return:
+ * list_len - function that returns the number of elements
+ * in a linked list_t list.
+ * @h: the first element of the list
+ * Return: the numbers of elements
  */
 
 size_t list_len(const list_t *h)
 {
+	size_t n = 0;
 
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
 }
