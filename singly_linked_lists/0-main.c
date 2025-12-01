18c75ebbf74f2f9f7cd13d66d6b3de2ff16062e3
GwenP88 <12777@holbertonstudents.com>
2025-12-01 14:11:00 +0100

    Project C - Singly linked lists - task 0 - V1

--- singly_linked_lists/0-main.c
+++ singly_linked_lists/0-main.c
@@ -13,28 +13,54 @@ int main(void)
     list_t *head;
     list_t *new;
     list_t hello = {"World", 5, NULL};
+	/* chaine hello, taille 5, pointeur sur le suivant null car fin de liste */
     size_t n;
 
-    head = &hello;
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
+    head = &hello; /*initialisation de la liste chainée avec 1 noeud */
+	/* head → [ "World" | 5 | NULL ] */
+
+    new = malloc(sizeof(list_t)); /*alloc dynamique pour ajout d'un nouveau noeud */
+	/* new → [ ??? | ??? | ??? ] */
+
+    if (new == NULL) /* test verif alloc OK */
     {
         printf("Error\n");
         return (1);
     }
-    new->str = strdup("Hello");
-    new->len = 5;
-    new->next = head;
+
+    new->str = strdup("Hello"); /* new ->str accede au champs str du noeud pointé par new */
+	/* new -> str = (*new).str */
+	/* strdup : fonction qui duplique une chaine de caractère */
+	/* new -> reçoit l'adresse de cette copie */
+
+    new->len = 5; /* taille de la chaine dupliquée */
+	/* new -> [ "Hello" | 5 | ??? ] */
+
+    new->next = head; /* accéde au pointeur vers le prochain noeud */
+	/* fait pointer new vers hello */
+	/* avant : head → [ "World" | 5 | NULL ] */
+	/* avant : new  → [ "Hello" | 5 | ??? ] */
+	/* aprés : new  → [ "Hello" | 5 |  &hello ] */
+	/* aprés : head → [ "World" | 5 | NULL ] */
+
     head = new;
+	/* liste a ce stade : head -> [ "Hello" | 5 |  &hello ]  →  [ "World" | 5 | NULL */
     n = print_list(head);
     printf("-> %lu elements\n", n);
+	/* parcour le liste, affiche les neoud, retourne le nb de noeud */
 
     printf("\n");
-    free(new->str);
-    new->str = NULL;
+	/* saut de ligne visuel */
+
+    free(new->str); /* libère la memoire alouée par strdup */
+	/* aprés free alloc, new->str ne pointe plus sur une zone valide */
+	new->str = NULL; /* annule le pointeur */
+
     n = print_list(head);
     printf("-> %lu elements\n", n);
+	/* parcour le liste, affiche les neoud, retourne le nb de noeud */
 
     free(new);
+	/* librere la structure du noeud new */
     return (0);
 }
--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,12 +1,31 @@
 #include "lists.h"
 
 /**
- * print_list -
- * @h:
- * Return:
+ * print_list - function that prints all the elements of a list_t list.
+ * @h: first elements of the list
+ * Return: the number of nodes
  */
 
 size_t print_list(const list_t *h)
 {
+	size_t n = 0;
+	const list_t *current;
 
+	if (h == NULL)
+		return (0);
+
+	current = h;
+
+	while (current != NULL)
+	{
+		if(current->str != NULL)
+		{
+			printf("[%d]", current->len);
+			printf("[%s]\n", current->str);
+		}else
+			printf("[0] (nil)\n");
+		n++;
+		current = current->next;
+	}
+	return (n);
 }
