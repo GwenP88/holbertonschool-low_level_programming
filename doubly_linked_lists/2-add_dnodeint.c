0e6bdda033d0a32e0add00ad2d69ee05d5cc363d
GwenP88 <12777@holbertonstudents.com>
2025-12-02 13:40:50 +0100

    Project C - doubly linked lists - task 2 - V1

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -9,5 +9,21 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *new;
 
+	/*verifier si head non nul*/
+	if (head == NULL)
+		return (NULL);
+
+	/*alloué de la mémoire pour new*/
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	*head = new;
+
+	return (new);
 }
