29336c0a498fbcbb3a03d54af041a4de6a5090ba
GwenP88 <12777@holbertonstudents.com>
2025-12-02 18:18:57 +0100

    Project C - doubly linked lists - task 8 - V1

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -9,5 +9,46 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *dl_node;
+	dlistint_t *prev_node;
+	dlistint_t *next_node;
+	dlistint_t *tmp;
+	unsigned int i = 0;
 
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		dl_node = *head;
+		tmp = dl_node->next;
+		if (tmp)
+		{
+			tmp->prev = NULL;
+			*head = tmp;
+		}
+		else
+			*head = NULL;
+		free(dl_node);
+		return (1);
+	}
+
+	dl_node = *head;
+	while (dl_node != NULL)
+	{
+		if (i == index)
+		{
+			next_node->prev = prev_node;
+			if (next_node == NULL)
+				prev_node->next = NULL;
+			else
+				prev_node->next = next_node;
+			free(dl_node);
+			return (1);
+		} else
+			return (-1);
+		dl_node = dl_node->next;
+		i++;
+	}
+	return (1);
 }
