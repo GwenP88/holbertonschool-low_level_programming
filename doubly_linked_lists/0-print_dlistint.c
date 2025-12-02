cdc99c8d89439af531e5375381d78523f241abf4
GwenP88 <12777@holbertonstudents.com>
2025-12-02 13:26:49 +0100

    Project C - doubly linked lists - task 0 - V1

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,5 +8,17 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
+	size_t i = 0;
+	const dlistint_t *current;
 
+	if (h == NULL)
+		return (0);
+	current = h;
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		i++;
+		current = current->next;
+	}
+	return (i);
 }
