f753016d05728b700bde8b5822a5ce80f4989986
GwenP88 <12777@holbertonstudents.com>
2025-12-03 16:12:47 +0100

    Project C - File-IO - task 0 - V1

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,12 +1,54 @@
 #include "main.h"
 
 /**
- * read_textfile -
- * @h:
- * Return:
+ * read_textfile - function that reads a text file
+ * and prints it to the POSIX standard output.
+ * @filename: a pointer to the file to read and display
+ * @letters: the number of letters it should read and print
+ * Return: the actual number of letters it could read and print
+ * 0 if the file can not be opened or read
+ * 0 if filename is NULL
+ * 0 if write fails or does not write the expected amount of bytes
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int file;
+	ssize_t bytes_read;
+	ssize_t bytes_written;
+	char *buffer;
 
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+
+	buffer = malloc(letters * (sizeof(char)));
+		if (buffer == NULL)
+		{
+			close(file);
+			return (0);
+		}
+
+	bytes_read = read(file, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(file);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			free(buffer);
+			close(file);
+			return (0);
+		}
+
+	free(buffer);
+	close(file);
+	return (bytes_written);
 }
