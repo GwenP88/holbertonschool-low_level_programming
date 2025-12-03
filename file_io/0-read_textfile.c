42ea750726e50c148fdac37aa1960c1c502abed3
GwenP88 <12777@holbertonstudents.com>
2025-12-03 15:26:09 +0100

    Project C - File-IO - init project

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * read_textfile -
+ * @h:
+ * Return:
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}
---
+++ file_io/1-create_file.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * create_file -
+ * @h:
+ * Return:
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+
+}
