2cb007d346173510bbe1d3cbb9b887be79caea4f
GwenP88 <12777@holbertonstudents.com>
2025-12-03 16:47:58 +0100

    Project C - File-IO - task 1 - V3

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int new_file;
-	int char_to_write = 0;
+	size_t char_to_write = 0;
 	ssize_t bytes_written;
 
 	if (filename == NULL)
@@ -34,11 +34,10 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[char_to_write] != '\0')
 	{
 		char_to_write++;
-		return (char_to_write);
 	}
 
 	bytes_written = write(new_file, text_content, char_to_write);
-		if (bytes_written != char_to_write || bytes_written == 0)
+		if (bytes_written != char_to_write)
 		{
 			close(new_file);
 			return (-1);
