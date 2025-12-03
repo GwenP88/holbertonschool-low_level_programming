d30e6e7e605d63ca9821fdbac206c0a9544750f4
GwenP88 <12777@holbertonstudents.com>
2025-12-03 16:43:58 +0100

    Project C - File-IO - task 1 - V2

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -26,15 +26,15 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		{
-			close(new_file);
-			return (1);
-		}
+	{
+		close(new_file);
+		return (1);
+	}
 
 	while (text_content[char_to_write] != '\0')
 	{
 		char_to_write++;
-		return(char_to_write);
+		return (char_to_write);
 	}
 
 	bytes_written = write(new_file, text_content, char_to_write);
