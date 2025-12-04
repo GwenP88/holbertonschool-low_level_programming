475f327cbdf298df273d74fea90adad9c1328add
GwenP88 <12777@holbertonstudents.com>
2025-12-04 09:48:47 +0100

    Project C - File-IO - task 3 - V2

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,33 +3,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
 int main(int argc, char *argv[])
 {
 	int file_from, file_to;
 	char buffer[1024];
-	ssize_t bytes_written, bytes_read;
+	ssize_t bytes_written, bytes_read = 0;
 
 	if (argc != 3)
-		{
-			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-			exit (97);
-		}
-
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit (98);
+		exit(98);
 	}
-
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit (99);
+		exit(99);
 	}
-
-	bytes_read = 0;
 	while ((bytes_read = read(file_from, buffer, 1024)) > 0)
 	{
 		bytes_written = write(file_to, buffer, bytes_read);
@@ -38,29 +40,25 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(file_from);
 			close(file_to);
-			exit (99);
+			exit(99);
 		}
 	}
-
 	if (bytes_read == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        close(file_from);
-        close(file_to);
-        exit (98);
-    }
-
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(file_from);
+		close(file_to);
+		exit(98);
+	}
 	if (close(file_from) == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close %s\n", argv[1]);
-			exit (100);
-		}
-
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close %s\n", argv[1]);
+		exit(100);
+	}
 	if (close(file_to) == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close %s\n", argv[2]);
-			exit (100);
-		}
-
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close %s\n", argv[2]);
+		exit(100);
+	}
 	return (0);
 }
