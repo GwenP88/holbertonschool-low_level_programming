783dc018a56ee6322a26e895521fd7621c52d8c0
GwenP88 <12777@holbertonstudents.com>
2025-12-04 10:24:18 +0100

    Project C - File-IO - task 3 - V3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,9 +3,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void close_file(int file);
+
 /**
  * main - check the code
- *
+ * @argc: number of arguments passed to the program
+ * @argv: array of argument strings (expects file_from and file_to)
  * Return: Always 0.
  */
 
@@ -13,7 +16,7 @@ int main(int argc, char *argv[])
 {
 	int file_from, file_to;
 	char buffer[1024];
-	ssize_t bytes_written, bytes_read = 0;
+	ssize_t bytes_written, bytes_read;
 
 	if (argc != 3)
 	{
@@ -50,15 +53,24 @@ int main(int argc, char *argv[])
 		close(file_to);
 		exit(98);
 	}
-	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close %s\n", argv[1]);
-		exit(100);
-	}
-	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close %s\n", argv[2]);
-		exit(100);
-	}
+	close_file(file_from);
+	close_file(file_to);
 	return (0);
 }
+
+/**
+ * close_file - closes a file descriptor and handles errors
+ * @file: file descriptor to be closed
+ *
+ * Return: This function does not return on failure.
+ */
+
+void close_file(int file)
+{
+	if (close(file) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close %d\n", file);
+		exit(100);
+	}
+}
+
