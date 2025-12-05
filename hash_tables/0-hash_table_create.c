5e325d9652ffcb592d076c8186b782d4da661c46
GwenP88 <12777@holbertonstudents.com>
2025-12-05 16:37:46 +0100

    project 4 - hash tables - task 2 - V1

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,18 +9,30 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	/* Pointeur vers la structure principale de la table de hachage */
 	hash_table_t *ht;
 
+	/* Allocation dynamique de la structure hash_table_t */
 	ht = malloc(sizeof(hash_table_t));
+	/* Si l'allocation échoue, on signale l'erreur au code appelant */
 	if (ht == NULL)
 		return (NULL);
 
+	/* On enregistre la taille de la table (nombre de cases du tableau) */
 	ht->size = size;
+
+	/*
+	 * Allocation du tableau de pointeurs vers hash_node_t.
+	 * calloc initialise toute la zone mémoire à 0 :
+	 * -> chaque élément du tableau (pointeur) sera mis à NULL.
+	 */
 	ht->array = calloc(size, sizeof(hash_node_t *));
 	if (ht->array ==  NULL)
 	{
+		/* En cas d’échec de cette deuxième allocation, on libère ht pour éviter une fuite mémoire */
 		free(ht);
 		return (NULL);
 	}
+	/* On renvoie la nouvelle table de hachage initialisée */
 	return (ht);
 }
--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -11,10 +11,19 @@ unsigned long int hash_djb2(const unsigned char *str)
 	unsigned long int hash;
 	int c;
 
+	/* Valeur de départ "magique" utilisée par l'algorithme djb2 */
 	hash = 5381;
+	/* Lit chaque caractère c jusqu'au '\0'*/
 	while ((c = *str++))
 	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+		/* Opération principale : hash = hash * 33 + c
+		 * (hash << 5) équivaut à hash * 32, donc (hash << 5) + hash = hash * 33
+		 * on “mélange” la valeur précédente du hash avec
+		 * ce caractère via une multiplication par 33
+		 * et une addition.
+		 */
+		hash = ((hash << 5) + hash) + c;
 	}
+	/* On renvoie la valeur finale du hash */
 	return (hash);
 }
