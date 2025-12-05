5acdabf2ae59591a56831f798a999844d23c20e2
GwenP88 <12777@holbertonstudents.com>
2025-12-05 16:06:04 +0100

    task 12

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * key_index -
+ * @size:
+ * @key:
+ * Return:
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+
+}
---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print -
+ * @ht:
+ * Return:
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+
+}
