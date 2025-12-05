4d8f572ab92451830772b7ab666e0b02971b37c2
GwenP88 <12777@holbertonstudents.com>
2025-12-05 16:18:01 +0100

    project 4 - hash tables - task 0 - V2

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -13,14 +13,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
-		return NULL;
+		return (NULL);
 
 	ht->size = size;
 	ht->array = calloc(size, sizeof(hash_node_t *));
 	if (ht->array ==  NULL)
 	{
 		free(ht);
-		return NULL;
+		return (NULL);
 	}
-	return ht;
+	return (ht);
 }
